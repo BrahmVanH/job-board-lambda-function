3c10acabf4596596b2d348428239181a3aab530c
Brahm Van Houzen <brahmvanh@gmail.com>
2025-09-30 18:47:40 -0400

    first commit. installed deps from ore dock cmms lambda function and started copying over boilerplate for rust graphql dynamodb lambda function

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::env;
+
+use aws_config::Region;
+use axum::{ extract::Extension, http::{ HeaderValue, Method }, routing::get, Router };
+use dotenvy::dotenv;
+use ore_dock_cmms_lambda::{
+    config::Config,
+    context::{ AppContext, ContextExtensions },
+    create_schema,
+    db,
+    AppError,
+    DbClient,
+    GraphQLSchema,
+};
+use tower::ServiceBuilder;
+use tower_http::{ compression::CompressionLayer, cors::{ Any, CorsLayer } };
+use async_graphql_axum::{ GraphQLRequest, GraphQLResponse };
+use serde::Serialize;
+use tracing::{ info, error };
+
+mod auth;
+
+fn main() {
+    println!("Hello, world!");
+}
